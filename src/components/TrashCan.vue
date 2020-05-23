702a4eb506decda762f2a112923b7c8db97bed3a
Gergely Muka <muka.gergely@gmail.com>
2020-05-23 08:59:26 +0200

    Create TrashCan component
    Also add some basic content and a little styling to it

---
+++ src/components/TrashCan.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <drop
+      @drop="handleDrop"
+    >
+      <div
+        class="drop-area"
+      >
+        PUT IT IN THE TRASHCAN
+      </div>
+    </drop>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TrashCan",
+  methods: {
+    handleDrop(data) {
+      console.log(data)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.drop-area {
+  background-color: rgba(43, 192, 110, 0.4);
+  display: block;
+  width: 100%;
+  height: 50px;
+}
+</style>
